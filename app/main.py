73457a4495f373584f1eb67971ba1cb5206ce566
Ahmed Sheta <as1209@student.london.ac.uk>
2023-03-23 22:32:25 +0200

    Pass second stage

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ from dataclasses import dataclass
 
 #import sqlparse
 
+import sqlite3
+
 database_file_path = sys.argv[1]
 command = sys.argv[2]
 
@@ -16,5 +18,11 @@ if command == ".dbinfo":
         database_file.seek(16)  # Skip the first 16 bytes of the header
         page_size = int.from_bytes(database_file.read(2), byteorder="big")
         print(f"database page size: {page_size}")
+
+        #Stage 2
+        database_file.seek(103)
+        row_num = int.from_bytes(database_file.read(2), byteorder='big')
+        print(f' number of rows: {row_num}"')
+
 else:
     print(f"Invalid command: {command}")
