eee4f1c20b1ee3a18ea392b2f4b1b743d37e341c
Ahmed Sheta <as1209@student.london.ac.uk>
2023-03-23 22:07:02 +0200

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import sys
 
 from dataclasses import dataclass
 
-# import sqlparse - available if you need it!
+#import sqlparse
 
 database_file_path = sys.argv[1]
 command = sys.argv[2]
@@ -13,8 +13,8 @@ if command == ".dbinfo":
         print("Logs from your program will appear here!")
 
         # Uncomment this to pass the first stage
-        # database_file.seek(16)  # Skip the first 16 bytes of the header
-        # page_size = int.from_bytes(database_file.read(2), byteorder="big")
-        # print(f"database page size: {page_size}")
+        database_file.seek(16)  # Skip the first 16 bytes of the header
+        page_size = int.from_bytes(database_file.read(2), byteorder="big")
+        print(f"database page size: {page_size}")
 else:
     print(f"Invalid command: {command}")
